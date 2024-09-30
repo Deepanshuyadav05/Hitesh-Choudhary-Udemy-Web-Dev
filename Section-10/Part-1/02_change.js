ce9d4e960e309ccf48188070101d65f7d3d77654
Deepanshu <137703927+Deepanshuyadav05@users.noreply.github.com>
2024-09-30 23:17:12 +0530

    change

---
+++ Section-10/Part-1/02_change.js
@@ -0,0 +1,11 @@
+let gameName = "spiderman"
+
+gameName = "batman"
+
+console.log(gameName)
+
+const username = "hitesh"
+
+//we cannot redefine const variable
+// username = "deep"
+console.log(username)
