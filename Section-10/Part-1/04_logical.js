975b566dd9fe91c8b753c7026f221ecb036fc354
Deepanshu <137703927+Deepanshuyadav05@users.noreply.github.com>
2024-09-30 23:18:00 +0530

    Logical operators

---
+++ Section-10/Part-1/04_logical.js
@@ -0,0 +1,16 @@
+// && => and
+// || => or
+// ! => not
+
+//and
+let isLoggedIn = true
+let isPaid = false
+
+console.log(isLoggedIn && isPaid)
+
+//or
+let isEmailUser = true
+let isGoogleUser = false
+
+console.log(isEmailUser || isGoogleUser)
+
