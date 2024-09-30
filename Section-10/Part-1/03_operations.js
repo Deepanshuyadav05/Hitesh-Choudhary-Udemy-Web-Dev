d5c41fdf8508203113c1ac2d3709552923490d5c
Deepanshu <137703927+Deepanshuyadav05@users.noreply.github.com>
2024-09-30 23:17:35 +0530

    Operations

---
+++ Section-10/Part-1/03_operations.js
@@ -0,0 +1,29 @@
+let totalScore = score+bonus
+let score = 102;
+let bonus = 25;
+
+console.log(totalScore)
+
+let addition = 3+7;
+let subtract = 3-7;
+let multiplication = 3*7;
+let division = 3/7;
+let power = 3**7;
+let remainder= 3%7;
+
+let myScore = 55
+myScore++
+myScore--
+
+//comparison operator
+
+let num1 = 3
+let num2 = 54
+let num3 = 89
+
+console.log(num1 == num2)
+console.log(num2 != num3)
+console.log(num2 < num3)
+console.log(num2 > num3)
+
+
