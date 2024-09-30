1fc912897e8c407d2c3f01d3bc68ede8005ef70e
Deepanshu <137703927+Deepanshuyadav05@users.noreply.github.com>
2024-09-30 23:16:38 +0530

    Data Types

---
+++ Section-10/Part-1/01_datatypes.js
@@ -0,0 +1,25 @@
+//to give multi line comments use ctrl+shift+/
+/*
+string
+Number
+Boolean
+BigInt
+
+Null
+Undefined
+
+symbol
+
+Object
+*/
+
+// var score = 102;
+
+let score = 102;
+let name = "chai"
+let isLoggedIn = false;
+
+//object
+let tea = ["lemon Tea", "orange tea", "masala Tea"];
+let user = {firstname: "John", lastname: "Doe", email: "john@doe.com"};
+
