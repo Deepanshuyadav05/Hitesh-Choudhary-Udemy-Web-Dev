6b1c676fbf6b5e456ef9df050092983105407e1d
Deepanshu <137703927+Deepanshuyadav05@users.noreply.github.com>
2024-10-01 09:40:10 +0530

    Operator precedence

---
+++ Section-10/Part-1/06_operator_precidence.js
@@ -0,0 +1,16 @@
+let score = 2 * 3 + 2 * 4;
+console.log(score)
+
+/*
+precedence
+()
+**
+* , / , %  (if all are present then start from left)
++ , -
+< , > , >= , <=  
+== , === , != , !==
+&&
+||
+? (ternary operator)
+= , += , -= , *= , /= , %=
+*/
